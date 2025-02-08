6e1828443e9ee0266b2488d692cf551694d674ce
IRomanchuk <iromanchuk06@gmail.com>
2025-02-08 19:19:38 +0300

    Init server tests

---
+++ server/src/test/java/com/example/volunteer_platform/server/ServerTests.java
@@ -0,0 +1,11 @@
+package com.example.volunteer_platform.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServerTests {
+    @Test
+    public void contextLoads() {
+    }
+}
