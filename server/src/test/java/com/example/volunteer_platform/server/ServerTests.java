f618242c3ab7b35394ef5ad4e37b5d25d08e4e61
IRomanchuk06 <iromanchuk06@gmail.com>
2025-02-25 21:56:05 +0300

    Use built-in database for tests

--- server/src/test/java/com/example/volunteer_platform/server/ServerTests.java
+++ server/src/test/java/com/example/volunteer_platform/server/ServerTests.java
@@ -2,8 +2,10 @@ package com.example.volunteer_platform.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ServerTests {
     @Test
     public void contextLoads() {
