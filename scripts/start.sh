2b829302dd15af1b1c5566ed35332cbee67d122f
IRomanchuk06 <iromanchuk06@gmail.com>
2025-02-25 23:11:13 +0300

    Update start script, fix paths, remove logs

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,22 +6,18 @@ if [ "$1" != "--no-build" ]; then
 fi
 
 echo "Stopping and removing existing containers..."
-docker-compose down
+docker-compose down > /dev/null 2>&1
 
 echo "Starting server and database..."
-docker-compose up -d server mysql
+docker-compose up -d server mysql > /dev/null 2>&1
 
-if ! docker-compose ps | grep mysql | grep Up; then
-  echo "Waiting for the database to be ready..."
-  ./wait-for-it.sh mysql:3306 --timeout=60 -- echo "Database is ready!"
-fi
+echo "Waiting for the database to be ready..."
+./scripts/wait-for-it.sh mysql:3306 --timeout=60 > /dev/null 2>&1
 
-if ! docker-compose ps | grep server | grep Up; then
-  echo "Waiting for the server to be ready..."
-  ./wait-for-it.sh server:8080 --timeout=60 -- echo "Server is ready!"
-fi
+echo "Waiting for the server to be ready..."
+./scripts/wait-for-it.sh server:8080 --timeout=60 > /dev/null 2>&1
 
 echo "Starting client in a new terminal..."
-gnome-terminal -- bash -c "docker-compose run client; exec bash"
+gnome-terminal -- bash -c "docker-compose run client 2>&1 | grep -v '^Creating\|^Starting\|^Attaching\|^ Container\|^Running\|^Healthy\|^wait-for-it.sh'; exec bash"
 
 echo "Server and database are running. Client is running in a separate terminal."
