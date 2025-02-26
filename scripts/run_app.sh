b2fcbdb0b65115eef24c6054eda362ffb114cd80
IRomanchuk06 <iromanchuk06@gmail.com>
2025-02-26 12:52:46 +0300

    Modify run_app script

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 VERBOSE=false
+BUILD=false
 
-if [[ "$1" == "--verbose" || "$1" == "-v" ]]; then
-  VERBOSE=true
-fi
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --verbose|-v)
+      VERBOSE=true
+      ;;
+    --build)
+      BUILD=true
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
 
 run_command() {
   if [ "$VERBOSE" = true ]; then
@@ -14,7 +27,7 @@ run_command() {
   fi
 }
 
-if [[ "$1" != "--no-build" && "$1" != "--verbose" && "$1" != "-v" ]]; then
+if [ "$BUILD" = true ]; then
   echo "Building Docker images..."
   run_command sudo docker compose build
 fi
